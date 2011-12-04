c3833d62b647b71c68ef1d6d51324708d0f5ae33
Jim Crossley <jim@crossleys.org>
2011-12-04 01:37:35 -0500

    Some useless commentary

--- src/overlay/core.clj
+++ src/overlay/core.clj
@@ -6,20 +6,31 @@
 
 (declare overlay-siblings)
 
-(defn zip-file [name]
+(defn zip-file
+  "Create a zipper from a filename"
+  [name]
   (zip/xml-zip (xml/parse (io/file name))))
 
-(defn zip-string [s]
+(defn zip-string
+  "Create a zipper from a string containing xml"
+  [s]
   (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-(defn stringify [zipper]
+(defn stringify
+  "Pretty print an xml zipper"
+  [zipper]
   (with-out-str (lazy-xml/emit (zip/root zipper) :indent 2)))
 
-(defn xml-node-equal [n p]
+(defn xml-node-equal
+  "Ignore the content attribute of XML nodes"
+  [n p]
   (and (= (:tag n) (:tag p))
        (= (:attrs n) (:attrs p))))
 
-(defn find-child [child {:keys [onto pred] :or {pred xml-node-equal}}]
+(defn find-child
+  "Find a matching child among the children of the overlay-ee"
+  [child {:keys [onto pred] :or {pred
+  xml-node-equal}}]
   (let [target (zip/node child)]
     (loop [cur (zip/down onto)]
       (cond
@@ -27,20 +38,26 @@
        (pred target (zip/node cur)) cur
        :else (recur (zip/right cur))))))
 
-(defn overlay-child [child {:keys [onto] :as args}]
+(defn overlay-child
+  "If matching child found, recursively overlay its
+   children. Otherwise append the child onto the overlay-ee."
+  [child {:keys [onto] :as args}]
   (let [found (find-child child args)]
     (if found
       (zip/up (overlay-siblings (zip/down child) (assoc args :onto found)))
       (zip/append-child onto (zip/node child)))))
 
-(defn overlay-siblings [child {:keys [onto ignore] :or {ignore #{}} :as args}]
+(defn overlay-siblings
+  "Overlay each sibling of the child onto the target"
+  [child {:keys [onto ignore] :or {ignore #{}} :as args}]
   (cond
    (nil? child) onto
    (ignore (zip/node child)) (recur (zip/right child) args)
    :else (recur (zip/right child) (assoc args :onto (overlay-child child args)))))
 
-(defn overlay [src & {:keys [onto] :as args}]
-  "Recursively overlay on one zipper with the nodes of another"
+(defn overlay
+  "Overlay one zipper onto another"
+  [src & {:keys [onto] :as args}]
   (overlay-siblings (zip/down src) args))
 
 
