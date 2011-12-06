f46565c4eddb48af1138b3645a0fd80b9b58d87c
Jim Crossley <jim@crossleys.org>
2011-12-05 23:28:14 -0500

    Add a test for file overlay

--- src/overlay/filesystem.clj
+++ src/overlay/filesystem.clj
@@ -1,14 +1,17 @@
 (ns overlay.filesystem
   (:require [clojure.java.io :as io]))
 
+(defn relative
+  "Return a File constructed with its path relative to a base path"
+  [file base]
+  (io/file (.substring (.getPath file) (+ 1 (count base)))))
+
 (defn overlay
   "Overlay the contents of one directory onto another"
   [src tgt]
-  (letfn [(relative [f]
-            (io/file (.substring (.getPath f) (+ 1 (count src)))))
-          (visit [dir]
+  (letfn [(visit [dir]
             (doseq [here (.listFiles dir)]
-              (let [there (io/file tgt (relative here))]
+              (let [there (io/file tgt (relative here src))]
                 (if (.isDirectory here)
                   (do (.mkdir there) (visit here))
                   (io/copy here there)))))]
---
+++ test/overlay/test/filesystem.clj
@@ -0,0 +1,39 @@
+(ns overlay.test.filesystem
+  (:use [overlay.filesystem])
+  (:require [clojure.java.io :as io])
+  (:use [clojure.test]))
+
+(def output-dir "test-resources/output/")
+
+(defn files
+  "A recursive listing of the relative filenames beneath dir"
+  [dir]
+  (letfn [(contents [d]
+            (reduce decide [] (.listFiles d)))
+          (decide [c x]
+            (if (.isDirectory x)
+              (concat c [x] (contents x))
+              (cons x c)))]
+    (sort (map #(str (relative % dir)) (contents (io/file dir))))))
+
+(defn delete-file-recursively
+  "rm -rf"
+  [f & [silently]]
+  (let [f (io/file f)]
+    (if (.isDirectory f)
+      (doseq [child (.listFiles f)]
+        (delete-file-recursively child silently)))
+    (io/delete-file f silently)))
+
+(use-fixtures :each (fn [f] (delete-file-recursively output-dir :quietly) (f)))
+
+;; (deftest no-output-dir
+;;   (println "no-output-dir")
+;;   (is (not (.exists (io/file output-dir)))))
+
+(deftest overlay-directory
+  (println "overlay-directory")
+  (let [from "test-resources/modules"
+        to (str output-dir "copy-of-modules")]
+    (overlay from to)
+    (is (= (files from) (files to)))))
