f6e45cf113b864dbe58fced93637e38163eab8b6
Jim Crossley <jim@crossleys.org>
2011-12-14 23:11:37 -0500

    Project metadata after first push to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject overlay "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject org.immutant/overlay "1.0.0-SNAPSHOT"
+  :description "Overlays modules from one JBoss AS7 installation onto another"
+  :url "http://github.com/immutant/overlay"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [dl "1.0.0"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]]
   :main overlay.core)
