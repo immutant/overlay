cc5cf73a19f270aa136ba769f792b117825c1461
Toby Crawley <toby@tcrawley.org>
2013-01-25 21:09:28 -0500

    Prepare for release.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject org.immutant/overlay "1.2.2"
+(defproject org.immutant/overlay "1.3.0"
   :description "Overlays features from one JBoss AS7 installation onto another"
   :url "http://github.com/immutant/overlay"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/data.json "0.2.0"]
