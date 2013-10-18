55c7bc7137a5c895fc2a9251da8775e2c1104d99
Toby Crawley <toby@tcrawley.org>
2013-10-18 10:54:01 -0400

    Release 1.5.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.immutant/overlay "1.5.2-SNAPSHOT"
+(defproject org.immutant/overlay "1.5.2"
   :description "Overlays features from one JBoss AS7 installation onto another"
   :url "http://github.com/immutant/overlay"
   :license {:name "Eclipse Public License"
